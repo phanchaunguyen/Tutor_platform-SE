46c0304c1a6f30bb1d62b4ffecfb24daa3cc6261
phanchaunguyen <phanchaunguyen4@gmail.com>
2026-03-27 16:41:14 +0700

    spring folder

---
+++ tutor-platform/tutor_platform_api/src/main/java/com/hcmut/tutor_platform_api/TutorPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.hcmut.tutor_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorPlatformApiApplication.class, args);
+	}
+
+}
---
+++ tutor-platform/tutor_platform_api/src/test/java/com/hcmut/tutor_platform_api/TutorPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmut.tutor_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
